48847fa0ae71b477507b37c84814ef8a4c61238f
frmn0404 <eprilianfirmansyah157@gmail.com>
2024-08-12 20:36:19 +0700

    perubahan pada login

---
+++ resources/views/components/head/tinymce-config.blade.php
@@ -0,0 +1,8 @@
+<script src="https://cdn.tiny.cloud/1/bnwlvij0qz24i55pc67spa7w693nrzaqgxc9z5fj1mkddj9x/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+  tinymce.init({
+    selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
+    plugins: 'code table lists',
+    toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+  });
+</script>
